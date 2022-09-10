1037fee0f12c1b45d9b980831b5b4f1f4565b65c
“Saulo <saulo_camargo_@hotmail.com>
2022-09-10 11:42:42 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
-  return(
+  return (
     <>
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container"></div>
+          <SalesCard />
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
